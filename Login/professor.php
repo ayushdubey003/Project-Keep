466efd1dec8186a982f041753d8f106da1f3cff1
ayushdubey003 <codeayush1998@gmail.com>
2019-02-05 11:29:51 +0530

    Complete frontend of login pages

---
+++ Login/professor.php
@@ -0,0 +1,45 @@
+<?php
+    require "../header.html";
+?>
+<html>
+    <head></head>
+    <body>
+    <style>
+        .inp
+        {
+            width:100%;
+            height:40px;
+            margin: 8px 0px;
+            padding: 12px 20px;
+            box-sizing: border-box;
+            border: 2px solid gray;
+            border-radius: 4px;
+        }
+        .submit
+        {
+            border-radius:2px;
+            margin-left: 34%;
+            background-color: #4CAF50;
+            color:white;
+            text-align: center;
+            padding: 10px 20px 10px 20px;
+            font-size: 15px;
+            border: none;
+            font-family:"../fonts/Futura_Light_font.ttf";
+        }
+        .submit:hover {background-color: #3e8e41}
+        .submit:active {
+            background-color: #3e8e41;
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
+        }
+    </style>
+        <div class="container" style="height:400px; width:300px;float: centre; margin-top: 100px; background-color:white ">
+            <form action="professor.php" method= "POST">
+                Username:<br><input type="text" name="username" class="inp"><br>
+                Password:<br><input type="password" name="username" class="inp"><br><br>
+                <input type="submit" value="Submit" name="submit" align:centre class="submit"><br>
+            </form>
+        </div>
+    </body>
+</html>
