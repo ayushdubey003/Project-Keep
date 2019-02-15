9cd29c348000312666886b7d06937d498a68c27f
Ayush Dubey <codeayush1998@gmail.com>
2019-02-15 13:34:03 +0530

    Add forgot password feature in students

---
+++ Login/checkpointstu.php
@@ -0,0 +1,7 @@
+<title>Password Reset Succesful</title>
+<?php
+    require "../header6.html";
+?>
+<div class="container" style="height:300px;width:40%;padding-top:10%">
+    <p style="align:centre;font-size:20px;font-family='fonts/Futura_Light_font.ttf'">A new password has been sent to the registerd mail id..</p>
+</div>
